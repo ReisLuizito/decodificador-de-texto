4dedb5e4c22ec5b82e054318df4669904397eb00
Luiz Guilherme Reis <luixguilherme@hotmail.com>
2023-05-26 22:37:47 -0300

    making semantical changes and advancing on javascript

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+// As "chaves" de criptografia que utilizaremos são:
+// `A letra "e" é convertida para "enter"`
+// `A letra "i" é convertida para "imes"`
+// `A letra "a" é convertida para "ai"`
+// `A letra "o" é convertida para "ober"`
+// `A letra "u" é convertida para "ufat"`
+
+
 function criptografar() {
-document.getElementById("mensagem").style.display = "none";
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
 }
